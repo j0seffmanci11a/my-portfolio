e7d2c873be3bf01e3ef2ecd16bec4057275b1bbf
j0seffmanci11a <0903jmm@gmail.com>
2025-04-18 12:16:11 -0400

    11th commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "j0seffmanci11a/my-portfolio/", // 👈 important!
+  base: "/my-portfolio/", // 👈 important!
   plugins: [react()],
 })
